b3ebc93f0a8d48ef1dfe1cebe733b6ef2295e431
fazzaamiarso <fazzaamiarso@gmail.com>
2024-07-22 19:16:10 +0700

    feat(domain): CommentRepository

---
+++ src/Domains/comments/CommentRepository.js
@@ -0,0 +1,7 @@
+class CommentRepository {
+  async addComment() {
+    throw new Error("ADD_COMMENT.METHOD_NOT_IMPLEMENTED");
+  }
+}
+
+module.exports = CommentRepository;
---
+++ src/Domains/comments/_test/CommentRepository.test.js
@@ -0,0 +1,11 @@
+const CommentRepository = require("../CommentRepository");
+
+describe("Comment interface", () => {
+  it("should throw error when invoke abstract behavior", async () => {
+    const commentRepository = new CommentRepository();
+
+    await expect(commentRepository.addComment({})).rejects.toThrowError(
+      "ADD_COMMENT.METHOD_NOT_IMPLEMENTED"
+    );
+  });
+});
